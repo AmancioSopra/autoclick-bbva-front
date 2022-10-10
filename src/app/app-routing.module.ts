99407dffd8fd133f7d06737db1c053f87f3f21ee
Amancio <aj.delgado@soprasteria.com>
2022-10-10 19:21:18 +0200

    LoginUser form Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginUserComponent } from './login-user/login-user.component';
 
-const routes: Routes = [];
+
+const routes:Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: LoginUserComponent},
+ 
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,26 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginUserComponent } from './login-user/login-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   LoginUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    ReactiveFormsModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
