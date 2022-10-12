74a3f4966fea688285dc00ccf1d71eeead3d4a69
Amancio <aj.delgado@soprasteria.com>
2022-10-12 22:28:24 +0200

    Added tests to LoginUserComponent

--- src/app/components/login-user/login-user.component.spec.ts
+++ src/app/components/login-user/login-user.component.spec.ts
@@ -7,7 +7,15 @@ import { LoginUserComponent } from './login-user.component';
 describe('LoginUserComponent', () => {
   let component: LoginUserComponent;
   let fixture: ComponentFixture<LoginUserComponent>;
-
+  const USER_DATA_MOCK =  {
+    name: "Amancio",
+    clickCount:5,
+    autoClickers: 0,
+  }
+  const USER_STORE_MOCK = {
+    users: [USER_DATA_MOCK],
+    currentUser: USER_DATA_MOCK.name
+  } 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -24,9 +32,91 @@ describe('LoginUserComponent', () => {
     fixture = TestBed.createComponent(LoginUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    let store:any = {};
+    const mockLocalStorage = {
+        getItem: (key: string): string => {
+          return key in store ? store[key] : null;
+        },
+        setItem: (key: string, value: string) => {
+          store[key] = `${value}`;
+        },
+        removeItem: (key: string) => {
+          delete store[key];
+        },
+        clear: () => {
+          store = {};
+        }
+      };
+    spyOn(localStorage, 'getItem')
+      .and.callFake(mockLocalStorage.getItem);
+    spyOn(localStorage, 'setItem')
+      .and.callFake(mockLocalStorage.setItem);
+    spyOn(localStorage, 'removeItem')
+      .and.callFake(mockLocalStorage.removeItem);
+    spyOn(localStorage, 'clear')
+      .and.callFake(mockLocalStorage.clear);
+      
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should return INVALID FORM if the email is not valid', () => {
+      const form = component.loginForm;
+      const inputName = form.controls['username'];
+      inputName.setValue("");
+      expect(form.invalid).toBe(true);
+      inputName.setValue("hola");
+      expect(form.invalid).toBe(true);
+  })
+  
+  it('should show alert message when form submited is invalid', () => {
+      const button = fixture.nativeElement.querySelector('#submitBtn');
+      spyOn(window, 'alert');
+      button.click();
+
+      expect(window.alert).toHaveBeenCalledWith('El formulario está mal cumplimentado');
+  })
+
+  it('should save data in localStorage when form is OK', () => {
+    const form = component.loginForm;
+      const inputName = form.controls['username'];
+      const button = fixture.nativeElement.querySelector('#submitBtn');
+      inputName.setValue("NuevoUsuario");
+      button.click();
+
+      expect(localStorage.getItem("currentUser")).toEqual("NuevoUsuario");
+  })
+  
+  it('should search the user in the localStorage when the user EXISTS', () => {
+    const form = component.loginForm;
+    const inputName = form.controls['username'];
+    const button = fixture.nativeElement.querySelector('#submitBtn');
+
+    localStorage.setItem('users', JSON.stringify([USER_DATA_MOCK]));
+    localStorage.setItem('currentUser', USER_DATA_MOCK.name);
+
+    inputName.setValue("Amancio");
+    button.click();
+    
+    expect(localStorage.getItem('users')?.length).not.toBe(0);
+    expect(localStorage.getItem('currentUser')).toEqual("Amancio");
+  })
+
+  it('should search the user in the localStorage when the user DOESNT EXISTS', () => {
+    const form = component.loginForm;
+    const inputName = form.controls['username'];
+    const button = fixture.nativeElement.querySelector('#submitBtn');
+
+    localStorage.setItem('users', JSON.stringify([USER_DATA_MOCK]));
+    localStorage.setItem('currentUser', USER_DATA_MOCK.name);
+
+    inputName.setValue("NuevoUsuario");
+    button.click();
+    
+    expect(localStorage.getItem('users')?.length).not.toBe(0);
+    expect(localStorage.getItem('currentUser')).toEqual("NuevoUsuario");
+  })
+})
